78bf1a27cd49ab7de7be6e06cdae1c94ebdfb958
Abutalha Sheikh <abutalhasheikh33@gmail.com>
2026-04-16 00:58:32 +0530

    feat: add Docker containerization, Redis client integration, and database seeding script

--- src/app/lib/redis.ts
+++ src/app/lib/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 let client: ReturnType<typeof createClient> | null = null;
-
+console.log(process.env.REDIS_URL);
 export async function getRedisClient() {
   if (!client) {
     client = createClient({
